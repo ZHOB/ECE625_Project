ed780ef1abab87e11e71ea5471f7072723c1c07e
WZihao <35311906+ZHOB@users.noreply.github.com>
2021-11-22 16:56:37 -0700

    Update project.R

--- project.R
+++ project.R
@@ -1,5 +1,12 @@
-dirc = "/Users/zhob/Desktop/ECE625/project/ECE625_Project/data/data.csv"
-data <- read.csv(dirc)
+dirc_zihao = "/Users/zhob/Desktop/ECE625/project/ECE625_Project/data/data.csv"
+
+dirc_xiaohan = ""
+
+dirc_luisa = ""
+
+## add your own local directory and change the follow to your own dirctory to use :)
+data <- read.csv(dirc_zihao)
+
 ## 90% of the sample size
 
 set.seed(2)
